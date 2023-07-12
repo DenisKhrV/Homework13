33448e583b81460a21f10fe240ed249a0bb8a9da
Денис <DenisKhr@yandex.ru>
2023-07-12 20:20:12 +1000

    Сделано ДЗ

--- src/Book.java
+++ src/Book.java
@@ -1,6 +1,8 @@
+import java.util.Objects;
+
 public class Book {
-    private String bookTitle;
-    Author authorName;
+    private final String bookTitle;
+    private final Author authorName;
     private int publicationYear;
 
     public Book(String bookTitle, Author authorName, int publicationYear) {
@@ -15,8 +17,28 @@ public class Book {
     public int getPublicationYear() {
         return this.publicationYear;
     }
+    public Author getAuthor() {
+        return this.authorName;
+    }
 
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
+    }
+
+    public String toString() {
+        return "Название книги: " + bookTitle + ". Автор книги: " + this.authorName + ". Дата издания: " + publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(bookTitle, book.bookTitle) && Objects.equals(authorName, book.authorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookTitle, authorName, publicationYear);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,29 @@ public class Main {
 
     public static void main(String[] args) {
         Author levTolstoy = new Author("Lev", "Tolstoy");
-        Book warAndPeace = new Book("War and Peace", levTolstoy, 1867);
+        Book warAndPeace = new Book("War and Peace", levTolstoy, 1447);
+        warAndPeace.setPublicationYear(1867);
         System.out.println("warAndPeace.getBookTitle() = " + warAndPeace.getBookTitle());
-        System.out.println("levTolstoy.getFirstName() = " + levTolstoy.getFirstName());
-        System.out.println("levTolstoy.getLastName() = " + levTolstoy.getLastName());
-        System.out.println("warAndPeace.getPublicationYear() = " + warAndPeace.getPublicationYear());
-        warAndPeace.setPublicationYear(2002);
+        System.out.println("warAndPeace.getAuthor() = " + warAndPeace.getAuthor().getFirstName());
+        System.out.println("warAndPeace.getAuthor().getLastName() = " + warAndPeace.getAuthor().getLastName());
         System.out.println("warAndPeace.getPublicationYear() = " + warAndPeace.getPublicationYear());
         System.out.println();
 
         Author nikolayNosov = new Author("Nikolay", "Nosov");
         Book neznaykaNaLune = new Book("Dunno on the Moon", nikolayNosov, 1965);
-        System.out.println("neznaykaNaLune.getBookTitle() = " + neznaykaNaLune.getBookTitle());
-        System.out.println("nikolayNosov.getFirstName() = " + nikolayNosov.getFirstName());
-        System.out.println("nikolayNosov.getLastName() = " + nikolayNosov.getLastName());
-        System.out.println("neznaykaNaLune.getPublicationYear() = " + neznaykaNaLune.getPublicationYear());
 
+        Author nikolayNosovCopy = new Author("Nikolay", "Nosov");
+        Book neznaykaNaLuneCopy = new Book("Dunno on the Moon", nikolayNosov, 1965);
+
+        System.out.println(warAndPeace);
+        System.out.println(neznaykaNaLune);
+        System.out.println();
+
+        if (nikolayNosov.equals(nikolayNosovCopy)) {
+            System.out.println("Автор одинаковый");
+        }
+        if (neznaykaNaLune.equals(neznaykaNaLuneCopy)) {
+            System.out.println("Книги полностью одинаковые");
+        }
     }
 }
